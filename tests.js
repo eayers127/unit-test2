127a25f094240e638676c608cd8bb4e7b793a7f1
Ethan Ayers <ethan.ayers88@gmail.com>
2023-04-19 20:33:24 -0600

    Wrote tests for math functions

--- tests.js
+++ tests.js
@@ -16,3 +16,30 @@ test('If number is not divisible by 3 or 5 should return the number', ()=>{
     expect(fizzBuzz(2).toBe('2'))
     expect(fizzBuzz(4).toBe('4'))
 });
+
+test('add two numbers together',()=>{
+    expect(addition(1,2).toBe(3))
+});
+
+test('subtract two numbers',()=>{
+    expect(subtract(4,2).toBe(2))
+});
+
+test('multiply two numbers together',()=>{
+    expect(addition(10,2).toBe(20))
+});
+
+test('divide one number by another',()=>{
+    expect(addition(10,2).toBe(5))
+});
+
+test('checks x to the nth power',()=>{
+    expect(power(6,2).toBe(36))
+});
+
+test('checks the square root of 49',()=>{
+    expect(root(49).toBe(7))
+});
+test('checks the square root of 63 and rounds it to the nearest whole number',()=>{
+    expect(root(63).toBe(8))
+});
